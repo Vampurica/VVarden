2527df7ece5add0df3867b00f93239e283bf6b41
SMVampire <vampuriccode@gmail.com>
2021-07-18 16:22:35 -0400

    tweak(config): Update Config

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
-let config = {
-    token:          "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+var config = {
+    token:          "",
     specialChar:	"!",
-    admin:			["282199104996507650","147409981757587456","548672876535087110"],
-    logChannel:     "861736808345370654",
+    admin:			[
+		"282199104996507650", // Vampire
+		"147409981757587456", // Mycroft
+		"548672876535087110", // Middle
+		"102498921640640512", // Leah
+		"695800069701632071", // Siege
+		"160347445711077376"  // Linden
+	],
+    logChannel:     "860961361898504202",
     addUsersChan:   "861665428760952852"
 };
 
