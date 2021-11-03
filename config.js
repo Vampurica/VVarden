524be72f931f3f2b3482844e79e098d07c56fee4
SMVampire <vampuriccode@gmail.com>
2021-11-03 01:00:48 -0400

    update(config): Adjust Staff & prettier

--- config.js
+++ config.js
@@ -11,17 +11,16 @@
     '640703664738271233', // Dunak
     '298207278375370763', // Eruzil
     '793379017062350888', // Kakarot
-	'215806999005364226', // DokaDoka
-	'778301104516890645', // Sondrix
+    '215806999005364226', // DokaDoka
+    '588473874644074517', // Kawan
   ]),
-
-module.exports = {
-  token: '',
-  logChannel: '',
-  addUsersChan: '',
-  host: '',
-  user: '',
-  password: '',
-  database: '',
-  spc: 'war '
-};
+  (module.exports = {
+    token: '',
+    logChannel: '',
+    addUsersChan: '',
+    host: '',
+    user: '',
+    password: '',
+    database: '',
+    spc: 'war ',
+  });
