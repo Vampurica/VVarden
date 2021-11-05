c9b58764961bec13515e9de350add90ba461fdd0
SMVampire <vampuriccode@gmail.com>
2021-11-05 12:50:27 -0400

    tweak(donate): Debrand Donate Message

--- commands/donate.js
+++ commands/donate.js
@@ -8,10 +8,9 @@ let donate = function () {
         embed: {
           title: 'Donate',
           description:
-            "If you would like to donate to me, or the CFX Community Improvement Project that created Warden, you can do so via Ko-Fi.\nDonations don't get you anything except for a thank you message in the donations channel of the discord.\nDonations made will be used to upkeep the bots and offset living costs of the creators and maintainers.\n\n**Ko-Fi**: <https://ko-fi.com/vampuric>",
+            "If you would like to donate to me, or the community that created Warden, you can do so via Ko-Fi.\nDonations don't get you anything except for a thank you message in the donations channel of the discord.\nDonations made will be used to upkeep the bots and offset living costs of the creators and maintainers.\n\n**Ko-Fi**: <https://ko-fi.com/vampuric>",
           color: 0x008000,
           footer: {
-            // Footer text
             text: 'VVarden by Vampire#8144',
           },
         },
